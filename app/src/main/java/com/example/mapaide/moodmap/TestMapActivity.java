327f219f743b030a8d0b4c1939ea80395727a49b
Kmarouane <khatar@ecole.ensicaen.fr>
2017-01-11 15:01:22 +0100

    Detection de l'emplacement et calcul de l'itinérair entre deux positions

--- app/src/main/java/com/example/mapaide/moodmap/TestMapActivity.java
+++ app/src/main/java/com/example/mapaide/moodmap/TestMapActivity.java
@@ -1,71 +1,103 @@
 package com.example.mapaide.moodmap;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.location.LocationManager;
-import android.util.Log;
+import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
+import org.osmdroid.bonuspack.routing.RoadNode;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.views.overlay.FolderOverlay;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polyline;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.ArrayList;
 
 public class TestMapActivity extends AppCompatActivity {
 
-    GeoPoint startPoint;
+    MapView map;
+    RoadManager roadManager;
+    ArrayList<GeoPoint> waypoints;
+
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_map);
         //important! set your user agent to prevent getting banned from the osm servers
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
 
-        MapView map = (MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
+        map = (MapView) findViewById(R.id.map);
+        //map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
-        //GeoPoint startPoint = null;//new GeoPoint(48.8583, 2.2944);
+        GeoPoint startPoint = new GeoPoint(49.1817, -0.3470);
 
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        //boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        /*if(isGPSEnabled) {
-            LocationListener locationListener = new LocationListener() {
-                public void onLocationChanged(Location location) {
-                    // Called when a new location is found by the network location provider.
-                    location.getLatitude();
-                    location.getLongitude();
-                    startPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
-                    String myLocation = "Latitude = " + location.getLatitude() + " Longitude = " + location.getLongitude();
-                    Log.e("MY CURRENT LOCATION", myLocation);
-                }
+        IMapController mapController = map.getController();
+        mapController.setZoom(12);
+        mapController.setCenter(startPoint);
+/*
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker.setDraggable(true);
+        map.getOverlays().add(startMarker);
+*/
+        GpsMyLocationProvider gpsLocationProvider = new GpsMyLocationProvider(this);
+        MyLocationNewOverlay myLocationOverlay = new MyLocationNewOverlay(gpsLocationProvider, map);
+        map.getOverlays().add(myLocationOverlay);
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.enableFollowLocation();
+/*
+        roadManager = new OSRMRoadManager(this);
+        waypoints = new ArrayList<GeoPoint>();
 
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-                }
+        waypoints.add(startPoint);
+        GeoPoint endPoint = new GeoPoint(49.1852, -0.3627);
+        waypoints.add(endPoint);
 
-                public void onProviderEnabled(String provider) {
-                }
+        new Thread(new Runnable() {
+            public void run() {
+                Road road = roadManager.getRoad(waypoints);
+                if (road.mStatus != Road.STATUS_OK);
+                    //Toast.makeText(TestMapActivity.class, "Error when loading the road - status=" + road.mStatus, Toast.LENGTH_SHORT).show();
+                Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+                map.getOverlays().add(roadOverlay);
+                FolderOverlay roadMarkers = new FolderOverlay();
+                map.getOverlays().add(roadMarkers);
+                Drawable nodeIcon = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_menu_camera, null);
+                for (int i = 0; i < road.mNodes.size(); i++) {
+                    RoadNode node = road.mNodes.get(i);
+                    Marker nodeMarker = new Marker(map);
+                    nodeMarker.setPosition(node.mLocation);
+                    nodeMarker.setIcon(nodeIcon);
 
-                public void onProviderDisabled(String provider) {
+                    //4. Filling the bubbles
+                    nodeMarker.setTitle("Step " + i);
+                    nodeMarker.setSnippet(node.mInstructions);
+                    nodeMarker.setSubDescription(Road.getLengthDurationText(TestMapActivity.this, node.mLength, node.mDuration));
+                    Drawable iconContinue = ResourcesCompat.getDrawable(getResources(), R.drawable.marker_default, null);
+                    nodeMarker.setImage(iconContinue);
+                    //4. end
+
+                    roadMarkers.add(nodeMarker);
                 }
-            };
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
 
             }
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        //}
-        IMapController mapController = map.getController();
-        mapController.setZoom(9);
-        mapController.setCenter(startPoint);
+        }).start();
+
 */
+
     }
 
     public void onResume(){
