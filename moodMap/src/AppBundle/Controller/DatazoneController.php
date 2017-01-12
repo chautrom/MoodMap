2dd7942139371093b9bdf645b5dbf33bc8ca2569
gwen14123 <gwendadou@hotmail.fr>
2017-01-12 14:38:08 +0100

    getInfoZones

--- moodMap/src/AppBundle/Controller/DatazoneController.php
+++ moodMap/src/AppBundle/Controller/DatazoneController.php
@@ -34,6 +34,64 @@ class DatazoneController extends Controller
 			array_push($data, $i);
 		}
 		
+		
+		
+		return new Response(json_encode($data));
+    }
+	
+	
+	/**
+     * @Route("/getInfoZones")
+     */
+    public function getInfoZonesAction()
+    {
+		
+        /**$em = $this->getDoctrine()->getManager();
+		
+		$requete=$em->createQuery('SELECT * FROM CRMCoreBundle:Zone z JOIN CRMCoreBundle:Datazone d')
+			->getResult();
+		
+		echo $requete;**/
+		
+		$datazones = $this->getDoctrine()
+			->getRepository('AppBundle:Datazone')
+			->findAll();
+			
+		$zones = $this->getDoctrine()
+			->getRepository('AppBundle:Zone')
+			->findAll();
+		
+		
+		$data = array();
+		foreach ($datazones as $itemDatazone) {
+			$d = array(
+				'id' => $itemDatazone->getId(),
+				'score' => $itemDatazone->getScore(),
+				'idCriteria' => $itemDatazone->getIdCriteria()
+			);
+			
+			foreach ($zones as $itemZone){
+				$z = array(
+					'id' => $itemZone->getId(),
+					'x' => $itemZone->getX(),
+					'y' => $itemZone->getY(),
+				);
+				if($d['id'] == $z['id']){
+					$res = array(
+						'x' => $itemZone->getX(),
+						'y' => $itemZone->getY(),
+						'score' => $itemDatazone->getScore(),
+						'idCriteria' => $itemDatazone->getIdCriteria()
+					);
+					array_push($data, $res);
+				};
+
+
+			};
+		};
+		
+		
+		
 		return new Response(json_encode($data));
     }
 
